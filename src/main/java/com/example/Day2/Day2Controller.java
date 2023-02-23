1440aff35631d9c95ceaf18a02dcbfbf5d06f6a6
deepakkannan7 <727721euit026@skcet.ac.in>
2023-02-23 12:07:41 +0530

    First commit

---
+++ src/main/java/com/example/Day2/Day2Controller.java
@@ -0,0 +1,17 @@
+package com.example.Day2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Day2Controller {
+
+	 @Value("${value}")
+	 private String name;
+	 @GetMapping("/ab")
+	 public String display()
+	 {
+		 return "Welcome to this "+name;
+	 }
+}
